10ce214c3b4993176b9bf51f61344d68608359fa
Gabszy10 <42463853+Gabszy10@users.noreply.github.com>
2025-10-11 02:23:22 +0800

    Align footer content with reservation details

--- blog.php
+++ blog.php
@@ -430,48 +430,39 @@
         <div class="footer_top">
             <div class="container">
                 <div class="row">
-                    <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="col-xl-4 col-md-6 col-lg-4">
                         <div class="footer_widget">
-                            <h3 class="footer_title">
-                                address
-                            </h3>
-                            <p class="footer_text"> P. Burgos Street, Barangay 1,<br>
-                                Batangas City, Philippines</p>
-                            <a href="#" class="line-button">Get Direction</a>
+                            <h3 class="footer_title">About St. Helena</h3>
+                            <p>P. Burgos Street<br>Barangay 1, Batangas City<br>
+                                Batangas 4200, Philippines<br>
+                                <a href="tel:+11234567890">(123) 456-7890</a><br>
+                                <a href="mailto:office@sthelenaparish.org">office@sthelenaparish.org</a>
+                            </p>
                         </div>
                     </div>
-                    <div class="col-xl-3 col-md-6 col-lg-3">
+                    <div class="col-xl-4 col-md-6 col-lg-4">
                         <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Reservation
-                            </h3>
-                            <p class="footer_text">+10 367 267 2678 <br>
-                                reservation@montana.com</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-md-6 col-lg-2">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Navigation
-                            </h3>
+                            <h3 class="footer_title">Quick Links</h3>
                             <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Rooms</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">News</a></li>
+                                <li><a href="reservation.php">Reserve a sacrament</a></li>
+                                <li><a href="schedule.php">Parish calendar</a></li>
+                                <li><a href="services.php">Ministry services</a></li>
+                                <li><a href="contact.php">Get in touch</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-4 col-md-6 col-lg-4">
                         <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Newsletter
-                            </h3>
-                            <form action="#" class="newsletter_form">
-                                <input type="text" placeholder="Enter your mail">
-                                <button type="submit">Sign Up</button>
-                            </form>
-                            <p class="newsletter_text">Subscribe newsletter to get updates</p>
+                            <h3 class="footer_title">Reservation Times</h3>
+                            <ul class="list-unstyled mb-0">
+                                <li><strong>Weddings:</strong> Monday – Saturday, 7:30 AM – 10:00 AM<br>
+                                    <span class="small text-muted">Additional 3:00 PM – 5:00 PM slot opens when the morning schedule fills.</span>
+                                </li>
+                                <li class="mt-2"><strong>Baptisms:</strong> Saturday &amp; Sunday, 11:00 AM – 12:00 PM</li>
+                                <li class="mt-2"><strong>Funerals:</strong> Sunday &amp; Monday at 1:00 PM or 2:00 PM<br>
+                                    <span class="small text-muted">Tuesday – Saturday at 8:00 AM, 9:00 AM, or 10:00 AM.</span>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -481,33 +472,10 @@
             <div class="container">
                 <div class="footer_border"></div>
                 <div class="row">
-                    <div class="col-xl-8 col-md-7 col-lg-9">
-                        <p class="copy_right">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">
+                            &copy; <?php echo date('Y'); ?> St. Helena Parish. All rights reserved.
                         </p>
-                    </div>
-                    <div class="col-xl-4 col-md-5 col-lg-3">
-                        <div class="socail_links">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-facebook-square"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-twitter"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-instagram"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
